119bcb9d49042e003f0c3b9fe0c234cc5ac43aec
Juan Collantes <juan@collantes.ec>
2024-06-28 00:26:17 -0500

    Punto de partida creado

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 2. Instalar dependencias `npm install`
 3. Clonar `env.template` y renombrar a `.env` y completar las variables de entorno en .env
 4. Levantar la base de datos `docker compose up -d`
-5. Generar el Prisma client `npx prisma generate`
-6. Ejecutar proyecto `npm run start:dev`
+5. crear la base de datos `employesDB`
+6. ejecutar el comando `npx prisma db pull` para traear el esquema a nest
+7. Generar el Prisma client `npx prisma generate`
+8. Ejecutar proyecto `npm run start:dev`
--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,14 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('DB Services');
   async onModuleInit() {
     await this.$connect();
-    // console.log('Connected to the database');
+    this.logger.log(`database conected`);
   }
 
   async hello() {
-    return this.employees.findFirst();
+    //return this.employees.findFirst();
+    return this.employees.findMany();
   }
 }
